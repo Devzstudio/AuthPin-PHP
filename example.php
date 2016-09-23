366788d2c19e924a2fefd641f77a9e32375a4666
Jijin P <pjijin1@gmail.com>
2016-09-23 21:01:28 +0530

    Example Usage

---
+++ example.php
@@ -0,0 +1,40 @@
+<?php
+// Your api code 
+$api = "";
+require_once(authpin.class.php);
+$authpin = new AuthPin($api);
+
+// Returns the QR code. Your user can this QR using AuthPin app / Website.
+$email = "dev@authpin.com";
+echo $authpin->register($email);
+
+// User Pin Verification
+$pin = "1234";
+if($authpin->verify($pin,$email))
+{
+ echo "Valid Pin";
+}
+else 
+{
+ echo "Invalid Pin";
+}
+
+
+// Send OTP to phone
+// Hash will be returned after OTP send . You need to pass the hash to verify the SMS
+$phone="9xxxxxxxxx";
+$country = "IN";
+$hash = $authpin->sendOTP($phone,$county);
+
+// Verify OTP Token
+$smscode = "1234";
+if($authpin->verifyotp( $smscode , $hash))
+{
+ echo "Valid OTP";
+}
+else 
+{
+ echo "Invalid OTP";
+}
+
+?>
